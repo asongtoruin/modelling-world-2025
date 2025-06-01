f7a0f6628c1e10f135d2da35cb5751d22c7246eb
Adam Ruszkowski <adam.ruszkowski@gmail.com>
2025-06-01 17:08:31 +0100

    Add QR code generation

---
+++ generate_qr_codes.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+from pathlib import Path
+
+import qrcode
+from qrcode.compat.etree import ET
+from qrcode.image.svg import SvgPathImage
+from qrcode.image.styles.moduledrawers.svg import SvgPathCircleDrawer
+
+
+# Force white-coloured QR codes
+SvgPathImage.QR_PATH_STYLE["fill"] = "#ffffff"
+
+links = {
+    "this_presentation": "https://asongtoruin.github.io/modelling-world-2025/",
+    "land-use": "https://github.com/Transport-for-the-North/Land-Use"
+}
+
+qrcodes_folder = Path("_static/qr_codes")
+qrcodes_folder.mkdir(exist_ok=True)
+
+for desc, link in links.items():
+
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data(link)
+
+    img = qr.make_image(
+        image_factory=SvgPathImage,
+        module_drawer=SvgPathCircleDrawer(size_ratio=Decimal(0.75)),
+        attrib={"href": link}
+    )
+
+    # Library doesn't seem to let us do a dimension-less SVG. Get the XML instead
+    svg = img._img
+
+    # Pop the height and width attrs
+    svg.attrib.pop("height")
+    svg.attrib.pop("width")
+
+    # Rather than img.to_string, we have to use the ET implementation
+    with (qrcodes_folder / f"{desc}.svg").open("w") as output_file:
+        output_file.write(ET.tostring(svg, encoding="unicode"))
